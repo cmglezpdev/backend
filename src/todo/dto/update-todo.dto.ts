f707dde03c244b2153bc58507b90439d654eaf94
Carlos Manuel González <cmglezpdev@gmail.com>
2023-10-12 15:34:45 -0400

    refact(Todo): update todo using abstract cruds

--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,12 +1,5 @@
 import { PartialType, InputType } from '@nestjs/graphql';
-import { Field, ID } from '@nestjs/graphql';
-import { IsUUID } from 'class-validator';
-
 import { CreateTodoDto } from './create-todo.dto';
 
 @InputType()
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {
-  @Field(() => ID)
-  @IsUUID()
-  id: string;
-}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) { }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,43 +1,21 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
 import { Todo } from './entities/todo.entity';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { AbstractCrudService } from 'src/abstract/abstract-crud.service';
 
 @Injectable()
-export class TodoService {
+export class TodoService extends AbstractCrudService<
+  Todo, CreateTodoDto, UpdateTodoDto
+> {
   constructor(
     @InjectRepository(Todo)
-    private readonly todoRepository: Repository<Todo>
-  ) { }
-
-  async getTodos(): Promise<Todo[]> {
-    return await this.todoRepository.find();
-  }
-
-  async getTodoById(id: string): Promise<Todo> {
-    const todo = await this.todoRepository.findOneBy({ id });
-    if (!todo) {
-      throw new BadRequestException(`Todo with id ${id} not found.`);
-    }
-    return todo;
-  }
-
-  async createTodo(createTodoDto: CreateTodoDto): Promise<Todo> {
-    const todo = this.todoRepository.create(createTodoDto);
-    return await this.todoRepository.save(todo);
-  }
-
-  async updateTodo(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
-    const todo = await this.todoRepository.findOneBy({ id });
-    const updatedTodo = this.todoRepository.merge(todo, updateTodoDto);
-    return this.todoRepository.save(updatedTodo);
-  }
-
-  async deleteTodoById(id: string): Promise<Todo> {
-    const todo = await this.todoRepository.findOneBy({ id });
-    return this.todoRepository.remove(todo);
+    todoRepository: Repository<Todo>
+  ) {
+    super();
+    this.repository = todoRepository;
   }
 }
